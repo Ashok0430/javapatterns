e91b9aabce3d142428fb4efe883d5f9512acfe1e
Ashok0430 <144242059+Ashok0430@users.noreply.github.com>
2023-09-06 19:04:33 +0530

    Add files via upload

---
+++ pattern09.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Pattern09
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter row number:");
+int row=sc.nextInt();
+System.out.println("enter column number:");
+int col=sc.nextInt();
+
+for(int i=1;i<=row;i++)
+{
+for(int j=1;j<=col;j++)
+{
+if((i+j==row+1)||(i==j))
+{
+System.out.print("#"+" ");
+}
+else
+{
+System.out.print(" *" +" ");
+}
+}
+System.out.println();
+}
+}
+}
---
+++ pattern10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Pattern10
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter row number:");
+int row=sc.nextInt();
+System.out.println("enter column number:");
+int col=sc.nextInt();
+
+for(int i=1;i<=row;i++)
+{
+for(int j=1;j<=col;j++)
+{
+if((i==1||j==1||i==row||j==col))
+{
+System.out.print("* ");
+}
+else
+{
+System.out.print("# ");
+}
+}
+System.out.println();
+}
+}
+}
